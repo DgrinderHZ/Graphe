d08390f68210b4220b480c265c7698d5ea8988e1
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-04-18 17:36:45 +0200

    Update functions.py

--- Python/functions.py
+++ Python/functions.py
@@ -24,3 +24,86 @@ def grapheOriente(G):
             if G[i][j] != G[j][i]:
                 return True
     return False
+
+
+
+def estSommetAdjacence(G, x, y):
+    if G[x][y] or G[y][x]:
+        return True
+    return False
+
+def estIsole(G, s):
+    for i in range(len(G)):
+        if G[s][i]:
+            return False
+    for i in range(len(G)):
+        if G[i][s]:
+            return False
+    return False
+
+
+
+def listeAdjacence(G):
+    listPrinc = []
+    for i in range(len(G)):
+        ilist = []
+        for j in range(len(G)):
+            if G[i][j]:
+                ilist.append(j);
+        listPrinc.append(ilist)
+    return listPrinc
+
+def listArc(G):
+    for i in range(len(G)):
+        for j in range(len(G)):
+            if G[i][j]:
+                print(i, " ---> ", j)
+        
+def fermetureTrans(G):
+    ft = G.copy()
+    for i in range(len(ft)):
+        for j in range(len(ft)):
+            if ft[j][i]:
+                for k in range(len(ft)):
+                    if(ft[i][k]):
+                        ft[j][k] += 1
+    return ft
+
+def estChaine(G, s, e):
+    FT = fermetureTrans(G)
+    if FT[s][e]:
+        return True
+    return False
+
+def listeAdjacenceRec(G, listPrinc):
+    i = len(listPrinc)
+    if i < len(G):
+        ilist = []
+        for j in range(len(G)):
+            if G[i][j]:
+                ilist.append(j);
+        listPrinc.append(ilist)
+        listeAdjacenceRec(G, listPrinc)
+
+def successeur(G, s):
+    ilist = []
+    for j in range(len(G)):
+        if G[s][j]:
+            ilist.append(j);
+    return ilist
+
+def parcoursEnLargeur(G, s):
+    #enfiler les successeur de s
+    myfile = []
+    marque = [0] * len(G)
+    myfile.extend(successeur(G, s))
+    #marquer s
+    marque[s]=1
+    print(s)
+    while len(myfile):
+        s = myfile.pop()
+        if marque[s] == 0:
+            myfile.extend(successeur(G, s))
+            #marquer s
+            marque[s]=1
+            print(s)
